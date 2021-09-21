4391a38d59f1f67b8ffbb57143270b9fd1aea714
Henrique Chagas <henriquechagasg@gmail.com>
2021-09-21 18:17:45 +0000

    Update env.ts

--- env.ts
+++ env.ts
@@ -14,11 +14,4 @@
 
 import Env from '@ioc:Adonis/Core/Env'
 
-export default Env.rules({
-  HOST: Env.schema.string({ format: 'host' }),
-  PORT: Env.schema.number(),
-  APP_KEY: Env.schema.string(),
-  APP_NAME: Env.schema.string(),
-  DRIVE_DISK: Env.schema.enum(['local'] as const),
-  NODE_ENV: Env.schema.enum(['development', 'staging', 'production'] as const),
-})
+export default Env.rules({})
