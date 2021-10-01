af5326d0f4b47480a766fe8c3b642768791db1a7
Henrique Chagas <henriquechagasg@gmail.com>
2021-10-01 16:03:16 -0300

    Changed on delete property for boards and lists

--- database/migrations/1631724578059_boards.ts
+++ database/migrations/1631724578059_boards.ts
@@ -7,7 +7,13 @@ export default class Boards extends BaseSchema {
     this.schema.createTable(this.tableName, (table) => {
       table.uuid('id').primary().unique().defaultTo(this.raw('uuid_generate_v4()')).notNullable()
       table.uuid('creator_id').unsigned().references('id').inTable('users').notNullable()
-      table.uuid('project_id').unsigned().references('id').inTable('projects').notNullable()
+      table
+        .uuid('project_id')
+        .unsigned()
+        .references('id')
+        .inTable('projects')
+        .notNullable()
+        .onDelete('CASCADE')
       table.string('name').notNullable()
 
       /**
--- database/migrations/1631724623519_lists.ts
+++ database/migrations/1631724623519_lists.ts
@@ -8,7 +8,13 @@ export default class Lists extends BaseSchema {
       table.uuid('id').primary().unique().defaultTo(this.raw('uuid_generate_v4()')).notNullable()
       table.string('name').notNullable()
       table.uuid('creator_id').unsigned().references('id').inTable('users').notNullable()
-      table.uuid('board_id').unsigned().references('id').inTable('boards').notNullable()
+      table
+        .uuid('board_id')
+        .unsigned()
+        .references('id')
+        .inTable('boards')
+        .notNullable()
+        .onDelete('CASCADE')
 
       /**
        * Uses timestamptz for PostgreSQL and DATETIME2 for MSSQL
