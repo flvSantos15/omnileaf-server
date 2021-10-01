dd881b6a89b830618651e89d059be18067ae42f1
Henrique Chagas <henriquechagasg@gmail.com>
2021-10-01 10:28:00 -0300

    Removed unused exceptions (changed this resposibility to validators)

--- app/Exceptions/CustomExceptionsHandlers/OrganizationExceptions.ts
+++
@@ -1,9 +0,0 @@
-import { Exception } from '@poppinss/utils'
-
-export default {
-  checkIfUserIsNotCreator: (creatorId: string, userId: string) => {
-    if (creatorId === userId) {
-      throw new Exception("Organization creator can't be deleted", 401)
-    }
-  },
-}
