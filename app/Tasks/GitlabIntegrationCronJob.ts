c563a8a8c2e02850b14f06a45e13cd34767ed815
henriquechagasg <henriquechagasg@gmail.com>
2021-11-19 07:05:22 -0300

    Added cron job feature

---
+++ app/Tasks/GitlabIntegrationCronJob.ts
@@ -0,0 +1,19 @@
+import { BaseTask } from 'adonis5-scheduler/build'
+import Logger from '@ioc:Adonis/Core/Logger'
+
+export default class GitlabIntegrationCronJob extends BaseTask {
+  public static get schedule() {
+    return '*/5 * * * * *'
+  }
+  /**
+   * Set enable use .lock file for block run retry task
+   * Lock file save to `build/tmpTaskLock`
+   */
+  public static get useLock() {
+    return false
+  }
+
+  public async handle() {
+    Logger.info('Handled')
+  }
+}
