8cb3be93e4fbc67a0c6e225da2556f571e5087ab
henriquechagasg <henriquechagasg@gmail.com>
2022-01-15 11:59:59 -0300

    Implemented tracking session reports

--- api/start/routes/Reports.ts
+++ api/start/routes/Reports.ts
@@ -2,6 +2,8 @@ import Route from '@ioc:Adonis/Core/Route'
 
 Route.group(() => {
   Route.get('reports/screenshots', 'ReportsController.screenshots')
+
+  Route.get('reports/tracking-sessions', 'ReportsController.trackingSessions')
 })
   .middleware('auth')
   .middleware('reports')
