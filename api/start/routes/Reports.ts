291d5ee2135f351a36fbf845efce25f2ab3d1cc2
henriquechagasg <henriquechagasg@gmail.com>
2022-01-18 18:33:25 -0300

    Temporarily removed auth middlewaree from reports route

--- api/start/routes/Reports.ts
+++ api/start/routes/Reports.ts
@@ -8,4 +8,4 @@ Route.group(() => {
   Route.get('reports/tracking-sessions', 'ReportsController.trackingSessions')
 
   Route.get('reports/time-and-activity', 'ReportsController.timeAndActivity')
-}).middleware('auth')
+})
