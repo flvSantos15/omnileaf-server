0552e17c679a7d2b1ee199419df5f01ee20e41ee
henriquechagasg <henriquechagasg@gmail.com>
2022-01-28 12:10:23 -0300

    Edited time and activity grouped report route and added swagger specifications

--- api/start/routes/Reports.ts
+++ api/start/routes/Reports.ts
@@ -1,14 +1,5 @@
 import Route from '@ioc:Adonis/Core/Route'
 
 Route.group(() => {
-  Route.get(
-    'reports/screenshots/time-and-activity',
-    'ReportsController.timeAndActivityOnScreenshotsReports'
-  )
-
-  Route.get('reports/screenshots/all', 'ReportsController.allScreenshots')
-
-  Route.get('reports/screenshots/every-ten-minutes', 'ReportsController.everyTenMinutesScreenshots')
-
-  Route.get('reports/time-and-activity', 'ReportsController.timeAndActivity')
+  Route.get('reports/time-and-activity/grouped', 'ReportsController.groupedReportOnTimeAndActivity')
 })
