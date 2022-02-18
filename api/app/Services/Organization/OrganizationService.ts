80714fdbaf1e50b14c7669af226727a4de9ca584
henriquechagasg <henriquechagasg@gmail.com>
2022-02-18 15:02:15 -0300

    Started dettachin user from organization projects when he is removed from organization

--- api/app/Services/Organization/OrganizationService.ts
+++ api/app/Services/Organization/OrganizationService.ts
@@ -6,6 +6,7 @@ import {
   UpdateOrganizationRequest,
 } from 'App/Interfaces/Organization/organization-service'
 import Organization from 'App/Models/Organization'
+import User from 'App/Models/User'
 
 class OrganizationService {
   public async getAll(): Promise<Organization[]> {
@@ -59,15 +60,32 @@ class OrganizationService {
   public async removeMember({ id, payload, bouncer }: RemoveMemberRequest): Promise<void> {
     const { userId } = payload
 
-    const organization = await Organization.find(id)
+    const [organization, user] = await Promise.all([Organization.find(id), User.find(userId)])
 
     if (!organization) {
-      throw new Exception('Organization Id does not exists', 404)
+      throw new Exception('Organization not found', 404)
+    }
+
+    if (!user) {
+      throw new Exception('User not found', 404)
     }
 
     await bouncer.authorize('OrganizationManager', organization)
 
+    await this._dettachUserFromProjects(user, organization)
+
     await organization.related('members').detach([userId])
+  }
+
+  private async _dettachUserFromProjects(user: User, organization: Organization) {
+    const projects = (await User.query()
+      .where('id', user.id)
+      .preload('assignedProjects', (query) => {
+        query.where('organizationId', organization.id)
+      })
+      .first())!.assignedProjects
+
+    await user.related('assignedProjects').detach(projects.map((project) => project.id))
   }
 }
 
